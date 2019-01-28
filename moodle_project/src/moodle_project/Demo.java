48b256892dcecc82e71d7fb54ec90da4e07cac9c
Deniz <denizmehmed97@gmail.com>
2019-01-28 13:46:59 +0200

    zdravei

--- moodle_project/src/moodle_project/Demo.java
+++ moodle_project/src/moodle_project/Demo.java
@@ -3,9 +3,10 @@ package moodle_project;
 public class Demo {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello world!");
-		
+		System.out.println("Zdr Ivo ");
+
 	}
 
 }
