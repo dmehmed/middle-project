73b2dbd88509a14d440834ae4fd6d49fd234897c
Deniz <denizmehmed97@gmail.com>
2019-01-28 21:01:47 +0200

    Merge problems hopefully resolved

--- moodle_project/src/system/System.java
+++ moodle_project/src/system/System.java
@@ -1,5 +1,8 @@
 package system;
 
+import courses.Course;
+import users.User;
+
 public class System {
 
 	private static System system_instance = null;
@@ -18,25 +21,25 @@ public class System {
 		return System.system_instance;
 	}
 
-	public static addUser(User user) {
+	public static void addUser(User user) {
 		if (user != null) {
 			System.usersStorage.add(user);
 		}
 	}
 
-	public static removeUser(User user) {
+	public static void removeUser(User user) {
 		if (user != null) {
 			System.usersStorage.remove(user);
 		}
 	}
 
-	public static addCourse(Course course) {
+	public static void addCourse(Course course) {
 		if (course != null) {
 			System.coursesStorage.add(course);
 		}
 	}
 
-	public static removeCourse(Course course) {
+	public static void (Course course) {
 		if (course != null) {
 			System.coursesStorage.remove(course);
 		}
